6b89619e5b0b5d51a7d0e0ad4a9ac080fc886fd3
Abhishek Kumar Singh <Abhisheksingh9135@gmail.com>
2019-02-07 14:44:22 +0530

    Implementation

---
+++ Jumping on the Clouds.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int c[] = new int[n];
+        for(int i=0; i < n; i++){
+            c[i] = in.nextInt();
+        }
+        int count = -1;
+        for (int i = 0; i < n; i++, count++) {
+            if (i<n-2 && c[i+2]==0) i++;
+        }
+        System.out.println(count);
+    }
+}
+
